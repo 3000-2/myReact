e9a0f887a97cb4cdc45d823e3e013c61fda0d00d
3000-2 <psch0493@gmail.com>
2022-09-25 13:40:57 +0900

    클래스 컴포넌트 만들어보기

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 /* @jsx createElement */
-import { createElement, render } from "./react";
+import { Component, createElement, render } from "./react";
 
-function Title(props) {
-  console.log(props);
-  return <h1 style={props.style}>{props.children}</h1>;
+class Title extends Component {
+  render() {
+    return <h1>{this.props.children}</h1>;
+  }
 }
+
+// function Title(props) {
+//   console.log(props);
+//   return <h1 style={props.style}>{props.children}</h1>;
+// }
 
 function Item(props) {
   return <li style={`color:${props.color}`}>{props.children}</li>;
 }
 
-const vDOM2 = (
+const App = () => (
   <p>
-    <Title style="color:red">리스트</Title>
+    <Title style="color:red">리스트 클래스</Title>
     <ul>
       <Item color={"blue"}>1</Item>
       <Item>2</Item>
@@ -21,4 +27,4 @@ const vDOM2 = (
   </p>
 );
 
-render(vDOM2, document.getElementById("root"));
+render(<App />, document.getElementById("root"));
--- src/react.js
+++ src/react.js
@@ -1,3 +1,9 @@
+export class Component {
+  constructor(props) {
+    this.props = props;
+  }
+}
+
 export const createDOM = (node) => {
   if (typeof node === "string") {
     return document.createTextNode(node);
@@ -18,15 +24,24 @@ export const createDOM = (node) => {
   return element;
 };
 
+const makeProps = (props, children) => {
+  return {
+    ...props,
+    children: children.length === 1 ? children[0] : children,
+  };
+};
+
 export const createElement = (tag, props, ...children) => {
   props = props || {};
 
   if (typeof tag === "function") {
+    if (tag.prototype instanceof Component) {
+      const instance = new tag(makeProps(props, children));
+
+      return instance.render();
+    }
     if (children.length > 0) {
-      return tag({
-        ...props,
-        children: children.length === 1 ? children[0] : children,
-      });
+      return tag(makeProps(props, children));
     }
     return tag(props);
   }
